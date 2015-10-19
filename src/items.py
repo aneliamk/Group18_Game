f1d2cf8d081f3aa6c0a7955e7369465a019edf43
jakeelliot1 <jakeelliotmorgan@gmail.com>
2015-10-19 12:00:10 +0100

    changes to syntax

--- src/items.py
+++ src/items.py
@@ -1,126 +1,79 @@
 item_id = {
     "id": "id",
-
     "name": "id card",
-
     "description":
     """You new shiny student ID card. Expires 1 June 2017.
 You wonder why they have printed a suicide hotline number on it?...""",
-
     "mass": 0.3,
-
     "health": 0,
-
     "alcohol_bar": 0
 }
 
 item_laptop = {
     "id": "laptop",
-
     "name": "laptop",
-
-    "description":
-    "It has seen better days. At least it has a WiFi card!",
-
+    "description": "It has seen better days. At least it has a WiFi card!",
     "mass": 2,
-
     "health": 0,
-
     "alcohol_bar": 0
 }
 
 item_money = {
     "id": "money",
-
     "name": "money",
-
-    "description":
-    "This wad of cash is barely enough to pay your tuition fees.",
-
+    "description": "This wad of cash is barely enough to pay your tuition fees.",
     "mass": 0.2,
-
     "health": 0,
-
     "alcohol_bar": 0
 }
 
 item_biscuits = {
     "id": "biscuits",
-
     "name": "a pack of biscuits",
-
     "description": "A pack of biscuits.",
-
     "mass": 0.5,
-
     "health": 0,
-
     "alcohol_bar": 0
 }
 
 item_pen = {
     "id": "pen",
-    
     "name": "a pen",
-
     "description": "A basic ballpoint pen.",
-
     "mass": 0.3,
-
     "health": 0,
-
     "alcohol_bar": 0
 }
 
 item_handbook = {
     "id": "handbook",
-    
     "name": "a student handbook",
-
     "description": "This student handbook explains everything. Seriously.",
-
     "mass": 0.7,
-
     "health": 0,
-
     "alcohol_bar": 0
 }
 
 item_tequila_shot = {
-    
     "id" : "tequila",
-
     "name" : "a Tequila shot",
-
     "description": "bla bla",
-
     "mass": 0,
-
     "health": -5,
-
     "alcohol_bar": 10,
-
     "price": 2
 }
-
-
 
 item_tequila_shot = {
-    
     "id" : "tequila",
-
     "name" : "a Tequila shot",
-
     "description": "bla bla",
-
     "mass": 0,
-
     "health": -5,
-
     "alcohol_bar": 10,
-
     "price": 2
 }
+
 item_WKD={
     "id": "WKD",
     "name": "a WKD",
@@ -130,6 +83,7 @@ item_WKD={
     "alcohol_bar": 10,
     "price": 3
 }
+
 item_Vodka={
     "id":"Vodka",
     "name":"a shot of Vodka",
@@ -139,6 +93,7 @@ item_Vodka={
     "alcohol_bar": 10,
     "price":2
     }
+
 item_Desperados={
     "id":"Desperados",
     "name":"a Desperados",
@@ -148,6 +103,7 @@ item_Desperados={
     "alcohol_bar": 10,
     "price":3
 }
+
 item_Jack_Daniels={
     "id":"Jack Daniels",
     "name":"Jack Daniels shot",
@@ -157,6 +113,7 @@ item_Jack_Daniels={
     "alcohol_bar": 10,
     "price":3
 }
+
 item_Water={
     "id":"Water",
     "name":"a glass of Water",
@@ -168,34 +125,22 @@ item_Water={
     "points":-1
 }
 
-item_sambuca_shot = {
-    
+item_sambuca_shot = {  
     "id" : "sambuca",
-
     "name" : "a Sambuca shot",
-
     "description": "A shot of Sambuca.",
     "mass": 0,
     "health": -10,
-
     "alcohol_bar": 10,
-    
     "price": 2
 }
 
-
-item_jagerbomb_shot = {
-    
+item_jagerbomb_shot = { 
     "id" : "jagerbomb",
-
     "name" : "a Jagerbomb shot",
-
     "description": "A shot of Jagerbomb.",
     "mass": 0,
     "health": -5,
-
     "alcohol_bar": 10,
-    
     "price": 2
 }
-
